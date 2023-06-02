d3619e4ceba1496d9fe87a95d23012b90e36412a
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-06-02 16:45:04 +0600

    added delete route to delete a task

--- index.js
+++ index.js
@@ -18,29 +18,39 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@crud-practice.heeny6h.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
-app.get('/tasks',async(req,res)=>{
+app.get('/tasks', async (req, res) => {
 
-    const collection=await client.db('Task-management').collection('task-crud');
-    const result=await collection.find({}).toArray();
+    const collection = await client.db('Task-management').collection('task-crud');
+    const result = await collection.find({}).toArray();
     res.send(result);
 });
 
 app.post('/add-task', async (req, res) => {
-    const task=req.body;
-    const collection=await client.db('Task-management').collection('task-crud');
+    const task = req.body;
+    const collection = await client.db('Task-management').collection('task-crud');
 
     const result = await collection.insertOne(task);
     res.send(result);
 });
+
+app.delete('/delete-task/:id', async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) }
+    const collection = await client.db('Task-management').collection('task-crud');
+    console.log(query)
+
+    const result = await collection.deleteOne(query);
+
+    res.send(result);
+})
